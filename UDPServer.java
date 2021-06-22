c5a5ad63e4967c6a0728fa4a4137c3b0091258dc
corsac-fox <keninfox@gmail.com>
2021-06-22 19:20:06 +0300

    this server can send messages to the client and receive messages from the client.

--- UDPServer.java
+++ UDPServer.java
@@ -1,59 +1,100 @@
+package tests;
+
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
-public class UDPServer {
-	
+public class UDPServer implements Runnable{
+
     public static final int PORT = 2005;
-	private DatagramSocket servSocket;
-	private boolean keepRunning = true;
-	String output;
-	
-	public static void main(String[] args) {
-		
+    private DatagramSocket servSocket;
+    private boolean keepRunning = true;
+    static InetAddress clientAddress;
+    static int clientPort;
+    private Thread sender;
+
+    public static void main(String[] args)
+    {
         UDPServer server = new UDPServer();
         server.service();
-   }
-	
-	public UDPServer()
-	{
-		try
-		{
+    }
+
+    public UDPServer()
+    {
+        try
+        {
             servSocket = new DatagramSocket(PORT);
-			
+            sender = new Thread(this);
+            sender.setDaemon(true);
+
         }
-		catch(SocketException e)
-		{
+        catch(SocketException e)
+        {
             System.err.println("Unable to open socket : " + e.toString());
-        }	
-	}
-	
-	protected void service() {
-		
+        }
+    }
+
+    protected void service() {
+
         DatagramPacket datagram;
         byte[] data;
-	  
+
         while (keepRunning)
-	    {
-		    try {
-                data = new byte[7];
+        {
+            try {
+
+                data = new byte[100];
                 datagram = new DatagramPacket(data, data.length);
                 servSocket.receive(datagram);
-			
-			    output = new String(data,  0, datagram.getLength());
-			    System.out.print(output + " ");
-			
-			    if (output.equals("end")) 
-			    {
-				    keepRunning = false;
-				    System.out.println("\nDatagram received");
-			    }
-		    
-			
+                //if (clientAddress == null || clientPort == 0)
+                {
+                    clientAddress = datagram.getAddress();
+                    clientPort = datagram.getPort();
+                    if (!sender.isAlive()) sender.start();
+
+                }
+
+                String output = new String(data,  0, datagram.getLength());
+                System.out.println(output);
+
             } catch(IOException e) {
-			 
+
                 System.err.println("I/O Exception : " + e.toString());
             }
         }
-	    servSocket.close();
+    }
+
+    @Override
+    public void run() //отправляет сообщения, когда узнает адрес клиента
+    {
+        Scanner scanner = new Scanner(System.in);
+        DatagramPacket packet;
+        while (keepRunning)
+        {
+
+            try
+            {
+                String input = scanner.nextLine();
+
+                if (input.equals("end"))
+                {
+                    keepRunning = false;
+                    System.out.println("\nDatagram received");
+                    servSocket.close();
+                    System.exit(0);
+                }
+
+                else if (clientAddress != null && input != "")
+                {
+                    byte[] byteArray = input.getBytes();
+                    packet = new DatagramPacket(byteArray, byteArray.length, clientAddress, clientPort);
+                    servSocket.send(packet);
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
